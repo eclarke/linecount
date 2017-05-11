00eb2ca9c5f485f885aa52205dd558e6128e7748
Erik Clarke <erikclarke@fastmail.com>
2017-05-11 15:54:13 -0400

    documentation

---
+++ README.md
@@ -0,0 +1,2 @@
+## Description
+Provides a function `count_lines` to count the number of lines in a file. The speed is comparable to GNU wc.
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,25 @@
+#![crate_name = "linecount"]
+#![deny(missing_docs,
+        missing_debug_implementations, missing_copy_implementations,
+        trivial_casts, trivial_numeric_casts,
+        unsafe_code,
+        unstable_features,
+        unused_import_braces, unused_qualifications)]
+
+//! Provides a fast line counting function.
 
 use std::io;
 use std::io::{BufRead, BufReader};
 
 const LF: u8 = '\n' as u8;
 
+/// Counts lines in the source `handle`. 
+/// 
+/// # Examples
+/// ```
+/// use linecount::count_lines
+/// let lines: usize = count_lines(std::fs::File.open("foo.txt").unwrap()).unwrap()
+/// ```
 pub fn count_lines<R: io::Read>(handle: R) -> Result<usize, io::Error> {
     let mut reader = BufReader::new(handle);
     let mut count = 0;
